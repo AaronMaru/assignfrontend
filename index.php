f6ca92d9f67d3a193de07f4d1287c256e95e3d7c
v.sai <v.sai@gl-f.com>
2018-01-19 17:25:21 +0700

    fix

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php include_once "home.html";?>
+<!doctype html>
+<html ng-app="AppCrud">
+
+<head>
+    <title>Customer</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+         <div ng-view></div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.6/angular.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.6/angular-route.js"></script>
+    <script src="js/app.js"></script>
+</body>
+
+</html>
